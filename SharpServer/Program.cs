8851cb7c29c4c6377a4680c657064303b1f7f8a2
aa5151521 <xinmunianhua@163.com>
2014-04-16 19:03:06 +0800

    add the c# server

---
+++ SharpServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpServer
+{
+    using ZeroMQ;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ZmqContext context = ZmqContext.Create())
+            {
+                using (ZmqSocket socket = context.CreateSocket(SocketType.PUB))
+                {
+                    socket.Bind("tcp://*:8585");
+
+                    while (true)
+                    {
+                        var msg = "test the zeromq";
+                        socket.Send(msg, Encoding.UTF8);
+
+                        Console.WriteLine("Send message:{0}", msg);
+                    }
+                }
+            }
+        }
+    }
+}
