521de22ab4eeb493b51e80b97dd9c1381e01f90d
aa5151521 <xinmunianhua@163.com>
2014-04-18 20:54:18 +0800

    Fix some bugs

--- SharpClient/Program.cs
+++ SharpClient/Program.cs
@@ -13,11 +13,11 @@
                 using (ZmqSocket socket = context.CreateSocket(SocketType.SUB))
                 {
                     socket.Connect("tcp://127.0.0.1:8585");
-                    socket.Subscribe(Encoding.Default.GetBytes("test"));
+                    socket.Subscribe(Encoding.Default.GetBytes("642"));
 
                     while (true)
                     {
-                        string rep = socket.Receive(Encoding.UTF8);
+                        string rep = socket.Receive(Encoding.GetEncoding("gb2312"));
                         Console.WriteLine("this is :{0}",rep);
                     }
                 }
